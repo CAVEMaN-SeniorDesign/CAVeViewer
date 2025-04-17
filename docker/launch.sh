00998d027fb782c2340db05301d93781216354c0
delta-12 <btesq235@gmail.com>
2025-04-16 20:06:43 -0400

    SD-353 change webserver root

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -e /etc/letsencrypt/live/caverobotics.com/cert.pem ]; then
-    certbot --nginx --agree-tos -m tyler.hansen028@gmail.com certonly --standalone --preferred-challenges http -d caverobotics.com --force-renewal --redirect --non-interactive
+    certbot --agree-tos -m tyler.hansen028@gmail.com certonly --nginx --preferred-challenges http -d caverobotics.com --force-renewal --redirect --non-interactive
 fi
 
 echo "0 0 * * 0 /usr/bin/certbot renew" >> certbot.renewal
