68c7751ff28bdee2bd4c6ca1d066b21653075638
Kushal Parekh <kup9@pitt.edu>
2025-08-15 16:04:32 -0400

    homepage progress bar approx, better UX'

--- src/components/gltf.jsx
+++ src/components/gltf.jsx
@@ -1,17 +1,30 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
-import { Html, useProgress } from '@react-three/drei'
+import { Html, useProgress } from '@react-three/drei';
 
 function Model() {
     // Use the useGLTF hook to load your model
     const { scene } = useGLTF("3d-assets/buddy.glb"); // Replace with your model's path
+    // const { scene } = useGLTF("https://threejs.org/examples/models/gltf/kira.glb");
     return <primitive object={scene} frustumCulled={false} rotation={[0, 0, Math.PI]} />;
 }
 
 function Loader() {
-  const { progress } = useProgress()
-  return <Html center className='buddyloader'>{progress} % loaded</Html>
+  const { progress } = useProgress();
+  periodic_progress_update = true;
+
+  // console.log(Math.round(progress));
+  // return <Html center className='buddyloader'>{progress} % loaded</Html>;
+  return (
+    <Html center className='testparentaccess' style={{ width: `100%`, height: "100%" }}>
+      <div className="progress" style={{ width: `100%` }}>
+          <div className="progress-bar progress-bar-striped progress-bar-animated" id="buddyprogressbar" style={{ width: `${Math.round(progress)}%`}}>
+              {Math.round(progress)}%
+          </div>
+      </div>
+    </Html>
+  );
 }
 
 
@@ -21,7 +34,7 @@ export function Gltfmesh() {
         <ambientLight intensity={30} />
         <directionalLight position={[10, 10, 10]} intensity={15} />
         <directionalLight position={[-10, -10, -10]} intensity={15} />
-        <Suspense fallback={<Loader />}>
+        <Suspense min-width="100%" fallback={<Loader />}>
           <Model />
         </Suspense>
         <OrbitControls position0={[0, 0, 95]} minDistance={95} maxDistance={95} autoRotate autoRotateSpeed={1.5} />
